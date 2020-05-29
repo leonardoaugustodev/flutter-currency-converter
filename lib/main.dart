1e7c9998878d27722c1d846503c5a66563826d09
Leonardo Santos <leonardoaugusto.dev@gmail.com>
2020-05-28 23:07:27 -0300

    First version

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,17 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=6d822d55";
 void main() async {
   //["results"]["currencies"]["USD"]
 
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber)),
+              hintStyle: TextStyle(color: Colors.amber)))));
 }
 
 Future<Map> getData() async {
@@ -22,6 +32,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dollarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dollar;
+  double euro;
+
+  void _clearAll(){
+    realController.text = "";
+    dollarController.text = "";
+    euroController.text = "";
+  }
+
+  void _realChanged(String text) {
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dollarController.text = (real / dollar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dollarChanged(String text) {
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double dollar = double.parse(text);
+    realController.text = (dollar * this.dollar).toStringAsFixed(2);
+    euroController.text = (dollar * this.dollar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dollarController.text = (dollar * this.dollar / euro).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,11 +105,44 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.center,
                   ));
                 } else {
-                  return Container(color: Colors.green);
+                  dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber),
+                          buildTextField(
+                              'Reais', 'R\$', realController, _realChanged),
+                          Divider(),
+                          buildTextField('Dollars', 'U\$', dollarController,
+                              _dollarChanged),
+                          Divider(),
+                          buildTextField(
+                              'Euros', '€', euroController, _euroChanged)
+                        ],
+                      ));
                 }
             }
-            
           }),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix,
+    TextEditingController controller, Function handleChange) {
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(fontSize: 25.0, color: Colors.amber),
+    controller: controller,
+    onChanged: handleChange,
+    keyboardType: TextInputType.number,
+  );
 }
